352f06c22848d58563fc349b47d0042e96531f5d
Tanmay <tanmaygautam1797@gmail.com>
2025-01-22 17:49:58 +0530

    shift mongodb to online server

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
-
+require("dotenv").config();
 //  define the mongodb connection URL
 
-const mongoURL = "mongodb://127.0.0.1:27017/hotels";
-
+// const mongoURL = process.env.MONGODB_URL_LOCAL;
+const mongoURL = process.env.MONGODB_URL;
 // set up mongodb  connection
 
 mongoose.connect(mongoURL);
